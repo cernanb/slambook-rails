9fca4f305c145a8175e6cf17868407853c566d80
Cernan Bernardo <cernanb@gmail.com>
2018-05-10 22:48:06 -0700

    Add custom attr writer for questions

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
 class Book < ApplicationRecord
+  has_many :questions
+  accepts_nested_attributes_for :questions
+
+  def questions_attributes=(hash)
+    hash.each do |k, v|
+      self.questions.build(content: v[:content])
+    end
+  end
 end
