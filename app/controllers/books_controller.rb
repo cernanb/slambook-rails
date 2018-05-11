55f99cc8f6b64ec28486d8b053aafbe6334e2fac
Cernan Bernardo <cernanb@gmail.com>
2018-05-10 22:49:02 -0700

    Add create show actions to bookscontroller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,21 @@
 class BooksController < ApplicationController
   def new
     @book = Book.new
+    @book.questions.build
   end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :creator, :questions_attributes => [:content])
+    end
 end
