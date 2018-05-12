02de2c5acb419d1899f3f8e2318d9538643313d7
Cernan Bernardo <cernanb@gmail.com>
2018-05-11 19:09:36 -0700

    Add listener for turbo links and conditional for question button

--- app/assets/javascripts/question.js
+++ app/assets/javascripts/question.js
@@ -1,16 +1,18 @@
-document.addEventListener('DOMContentLoaded', bindListeners, false)
+document.addEventListener('turbolinks:load', bindListeners, false)
 
 function bindListeners() {
   const questionButton = document.querySelector('#add-question')
   const questions = document.getElementById('questions')
-  let questionIndex = 1
-  questionButton.addEventListener('click', e => {
-    e.preventDefault()
-    var newEl = document.createElement('input')
-    newEl.setAttribute('type', 'text')
-    newEl.setAttribute('name', `book[questions_attributes][${questionIndex}][content]`)
-    questionIndex += 1
-    // newEl.appendChild(document.createTextNode('Hello World!'))
-    questions.appendChild(newEl)
-  })
+  if (questionButton && questions) {
+    let questionIndex = 1
+    questionButton.addEventListener('click', e => {
+      e.preventDefault()
+      var newEl = document.createElement('input')
+      newEl.setAttribute('type', 'text')
+      newEl.setAttribute('name', `book[questions_attributes][${questionIndex}][content]`)
+      questionIndex += 1
+      // newEl.appendChild(document.createTextNode('Hello World!'))
+      questions.appendChild(newEl)
+    })
+  }
 }
