4213c1acf3ab28ed6204060a4b1b8541b1e80a37
Cernan Bernardo <cernanb@gmail.com>
2018-05-11 19:08:41 -0700

    Add root route and next route for question

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :books, only: [:new, :create, :show] do
+  root 'application#home'
+  resources :books, only: [:new, :create, :show, :index] do
     resources :questions, only: [:show]
+    get '/questions/:id/next', to: 'questions#next', as: :next_question
   end
 end
