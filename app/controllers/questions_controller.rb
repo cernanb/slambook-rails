554d57f4cb56f8eda90052afb30ac4ef93d2ea8d
Cernan Bernardo <cernanb@gmail.com>
2018-05-11 19:39:59 -0700

    Add next action to questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,11 @@ class QuestionsController < ApplicationController
     @book = Book.find(params[:book_id])
     @question = Question.find(params[:id])
   end
+  
+  def next
+    @book = Book.find(params[:book_id])
+    @question = Question.find(params[:id])
+    next_question = @book.questions.where('id > ?', @question.id).first
+    redirect_to book_question_path(@book, next_question)
+  end
 end
