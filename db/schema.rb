e7608c3abb89c8a9cac7af5001938f027d95786b
Cernan Bernardo <cernanb@gmail.com>
2018-06-26 23:10:01 -0700

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180511035312) do
+ActiveRecord::Schema.define(version: 20180627060332) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text "content"
+    t.integer "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
